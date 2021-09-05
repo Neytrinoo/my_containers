8845a069928b0552d47d93da46b0d70888151b1c
Dmitriy Ilyin <neytrino070902@yandex.ru>
2021-09-05 20:13:13 +0300

    separated the signatures from the implementation

--- container.cpp
+++ container.cpp
@@ -4,66 +4,17 @@
 #include "container.h"
 
 
-template<typename T, typename Alloc = std::allocator<T>>
-class Container {
-    size_t length_;
-    size_t capacity_;
-    Alloc allocator_;
+template<typename T, typename Alloc>
+size_t Container<T, Alloc>::get_capacity() {
+    return capacity_;
+}
 
+template<typename T, typename Alloc>
+size_t Container<T, Alloc>::get_length() {
+    return length_;
+}
 
-    // overloaded operators
-    virtual Container &operator=(const Container &other);
-
-    virtual bool operator==(const Container &other);
-
-
-    // access to elements
-    virtual Container &at(size_t ind);
-
-    virtual Container &operator[](size_t ind);
-
-    virtual Container &front();
-
-    virtual Container &back();
-
-
-    // TODO: iterators
-
-    // working with the size
-
-    size_t get_capacity() {
-        return capacity_;
-    }
-
-    size_t get_length() {
-        return length_;
-    }
-
-    bool is_empty() {
-        return length_ == 0;
-    }
-
-    virtual bool reserve(size_t count_elements); // returns true if reserve passed successfully
-
-    virtual bool resize(size_t count_elements); // returns true if resize passed successfully
-
-    virtual void shrink_to_fit();
-
-
-    // modifiers
-
-    virtual void clear();
-
-    virtual bool insert(size_t ind, const T &value);
-
-    virtual bool erase(size_t ind);
-
-    virtual bool erase(size_t from, size_t to);
-
-    virtual void push_back(const T &element);
-
-    virtual void pop_back();
-
-    virtual void swap(const T& other);
-
-};
+template<typename T, typename Alloc>
+bool Container<T, Alloc>::is_empty() {
+    return length_ == 0;
+}
--- container.h
+++ container.h
@@ -1,3 +1,61 @@
 #pragma once
 
 
+template<typename T, typename Alloc = std::allocator<T>>
+class Container {
+    size_t length_;
+    size_t capacity_;
+    Alloc allocator_;
+
+
+    // overloaded operators
+    virtual Container &operator=(const Container &other);
+
+    virtual bool operator==(const Container &other);
+
+
+    // access to elements
+    virtual Container &at(size_t ind);
+
+    virtual Container &operator[](size_t ind);
+
+    virtual Container &front();
+
+    virtual Container &back();
+
+
+    // TODO: iterators
+
+    // working with the size
+
+    size_t get_capacity();
+
+    size_t get_length();
+
+    bool is_empty();
+
+    virtual bool reserve(size_t count_elements); // returns true if reserve passed successfully
+
+    virtual bool resize(size_t count_elements); // returns true if resize passed successfully
+
+    virtual void shrink_to_fit();
+
+
+    // modifiers
+
+    virtual void clear();
+
+    virtual bool insert(size_t ind, const T &value);
+
+    virtual bool erase(size_t ind);
+
+    virtual bool erase(size_t from, size_t to);
+
+    virtual void push_back(const T &element);
+
+    virtual void pop_back();
+
+    virtual void swap(const T &other);
+
+};
+
